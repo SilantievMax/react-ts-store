8b19ee7f2b22a57ea8c2f4355e04b9810e13c58f
SilantievMax <nohrin.max@yandex.ru>
2023-04-06 21:52:25 +0300

    modified header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
   return (
-    <header>
+    <header className={styles.header}>
       <Layout>
         <TopHeader />
       </Layout>
